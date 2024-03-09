fc6193f0e027e44b26b879723b0c56ae56f5e7f1
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-03-09 14:15:57 +0000

    config: install and config react query

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -1,7 +1,10 @@
 import { ipcMain } from 'electron'
 
-ipcMain.handle('fetch-documents', async (_, params) => {
-  console.log(params)
-
-  return 'Hello World'
+ipcMain.handle('fetch-documents', async () => {
+  return [
+    { id: '1', title: 'Front-end' },
+    { id: '2', title: 'Back-end' },
+    { id: '3', title: 'Database' },
+    { id: '4', title: 'Deployment' },
+  ]
 })
