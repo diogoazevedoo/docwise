502de1abc569c70a28904b005359f8c96abad403
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-03-09 16:32:55 +0000

    refactor: create constants for ipc function and unique file for types

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -1,10 +1,17 @@
 import { ipcMain } from 'electron'
+import { IPC } from '../shared/constants/ipc'
+import { FetchAllDocumentsResponse } from '../shared/types/ipc'
 
-ipcMain.handle('fetch-documents', async () => {
-  return [
-    { id: '1', title: 'Front-end' },
-    { id: '2', title: 'Back-end' },
-    { id: '3', title: 'Database' },
-    { id: '4', title: 'Deployment' },
-  ]
-})
+ipcMain.handle(
+  IPC.DOCUMENTS.FETCH_ALL,
+  async (): Promise<FetchAllDocumentsResponse> => {
+    return {
+      data: [
+        { id: '1', title: 'Front-end', content: '' },
+        { id: '2', title: 'Back-end', content: '' },
+        { id: '3', title: 'Database', content: '' },
+        { id: '4', title: 'Deployment', content: '' },
+      ],
+    }
+  },
+)
