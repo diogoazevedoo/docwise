06b197bf4669321cdf459af142f40d68c6e6c7c3
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-03-09 17:06:01 +0000

    store: create all handlers and connected to store

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -1,17 +1,71 @@
 import { ipcMain } from 'electron'
+import { randomUUID } from 'node:crypto'
 import { IPC } from '../shared/constants/ipc'
-import { FetchAllDocumentsResponse } from '../shared/types/ipc'
+import {
+  CreateDocumentResponse,
+  DeleteDocumentRequest,
+  FetchAllDocumentsResponse,
+  FetchDocumentRequest,
+  FetchDocumentResponse,
+  SaveDocumentRequest,
+  Document,
+} from '../shared/types/ipc'
+import { store } from './store'
 
 ipcMain.handle(
   IPC.DOCUMENTS.FETCH_ALL,
   async (): Promise<FetchAllDocumentsResponse> => {
     return {
-      data: [
-        { id: '1', title: 'Front-end', content: '' },
-        { id: '2', title: 'Back-end', content: '' },
-        { id: '3', title: 'Database', content: '' },
-        { id: '4', title: 'Deployment', content: '' },
-      ],
+      data: Object.values(store.get('documents')),
     }
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.FETCH,
+  async (_, { id }: FetchDocumentRequest): Promise<FetchDocumentResponse> => {
+    const document = store.get(`documents.${id}`) as Document
+
+    return {
+      data: document,
+    }
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.CREATE,
+  async (): Promise<CreateDocumentResponse> => {
+    const id = randomUUID()
+
+    const document: Document = {
+      id,
+      title: 'Untitled',
+      content: '',
+    }
+
+    store.set(`documents.${id}`, document)
+
+    return {
+      data: document,
+    }
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.SAVE,
+  async (_, { id, title, content }: SaveDocumentRequest): Promise<void> => {
+    store.set(`documents.${id}`, {
+      id,
+      title,
+      content,
+    })
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.DELETE,
+  async (_, { id }: DeleteDocumentRequest): Promise<void> => {
+    // @ts-expect-error (https://github.com/sindresorhus/electron-store/issues/196)
+    store.delete(`documents.${id}`)
   },
 )
