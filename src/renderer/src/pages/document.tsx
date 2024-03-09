aa3f29e11de484fb6e8e08abe046a9dbf101dffe
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-03-09 10:25:49 +0000

    UI: started creating ui schema

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,10 +1,25 @@
-import { Link } from 'react-router-dom'
+import { ToC } from '../components/ToC'
 
 export function Document() {
   return (
-    <main className="flex-1 flex items-center justify-center text-docwise-400">
-      Document
-      <Link to="/">Access blank</Link>
+    <main className="flex-1 flex py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-docwise-300 font-semiboldbold text-xs">
+          TABLE OF CONTENT
+        </span>
+
+        <ToC.Root>
+          <ToC.Link>Back-end</ToC.Link>
+          <ToC.Section>
+            <ToC.Link>Database</ToC.Link>
+            <ToC.Link>Authentication</ToC.Link>
+          </ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className="flex-1 flex flex-col items-center">
+        wdqdqdqdqd
+      </section>
     </main>
   )
 }
