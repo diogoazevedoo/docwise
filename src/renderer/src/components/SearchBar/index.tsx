18b63ebcaabd673d7d6b98291fc73b744f2f8535
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-03-10 19:21:16 +0000

    feat: implemented searchbar using cmdk library

--- src/renderer/src/components/SearchBar/index.tsx
+++ src/renderer/src/components/SearchBar/index.tsx
@@ -1,60 +1,72 @@
+import { useQuery } from '@tanstack/react-query'
 import { Command } from 'cmdk'
 import { File, MagnifyingGlass } from 'phosphor-react'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-export function SearchBar() {
-  const [open, setOpen] = useState(false)
+interface SearchBarProps {
+  open: boolean
+  onOpenChange: (isOpen: boolean) => void
+}
+
+export function SearchBar({ open, onOpenChange }: SearchBarProps) {
+  const navigate = useNavigate()
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
       if (e.key === 'k' && e.metaKey) {
-        setOpen((state) => !state)
+        onOpenChange(!open)
       }
     }
 
     document.addEventListener('keydown', down)
     return () => document.removeEventListener('keydown', down)
-  }, [setOpen])
+  }, [onOpenChange, open])
+
+  const { data } = useQuery(['documents'], async () => {
+    const response = await window.api.fetchDocuments()
+
+    return response.data
+  })
+
+  function handleOpenDocument(id: string) {
+    navigate(`/documents/${id}`)
+
+    onOpenChange(false)
+  }
 
   return (
     <Command.Dialog
       className="fixed top-24 left-1/2 -translate-x-1/2 w-[480px] max-w-full bg-docwise-800 rounded-md shadow-2xl text-docwise-100 border border-docwise-600"
       open={open}
-      onOpenChange={setOpen}
+      onOpenChange={onOpenChange}
       label="Search"
     >
       <div className="flex items-center gap-2 border-b border-docwise-700 p-4">
         <MagnifyingGlass className="w-5 h-5" />
         <Command.Input
           autoFocus
-          placeholder="Buscar documentos..."
+          placeholder="Search documents..."
           className="w-full bg-transparent focus:outline-none text-sm text-docwise-50 placeholder:text-docwise-200"
         />
       </div>
       <Command.List className="py-2 max-h-48 scrollbar-thin scrollbar-thumb-docwise-600 scrollbar-track-docwise-800">
         <Command.Empty className="py-3 px-4 text-docwise-200 text-sm">
-          Nenhum documento encontrado.
+          No document found.
         </Command.Empty>
 
-        <Command.Item className="py-3 px-4 text-docwise-50 text-sm flex items-center gap-2 hover:bg-docwise-700 aria-selected:!bg-docwise-600">
-          <File className="w-4 h-4" />
-          Untitled
-        </Command.Item>
-
-        <Command.Item className="py-3 px-4 text-docwise-50 text-sm flex items-center gap-2 hover:bg-docwise-700 aria-selected:!bg-docwise-600">
-          <File className="w-4 h-4" />
-          Ignite
-        </Command.Item>
-
-        <Command.Item className="py-3 px-4 text-docwise-50 text-sm flex items-center gap-2 hover:bg-docwise-700 aria-selected:!bg-docwise-600">
-          <File className="w-4 h-4" />
-          Discover
-        </Command.Item>
-
-        <Command.Item className="py-3 px-4 text-docwise-50 text-sm flex items-center gap-2 hover:bg-docwise-700 aria-selected:!bg-docwise-600">
-          <File className="w-4 h-4" />
-          Rocketseat
-        </Command.Item>
+        {data?.map((document) => {
+          return (
+            <Command.Item
+              key={document.id}
+              onSelect={() => handleOpenDocument(document.id)}
+              className="py-3 px-4 text-docwise-50 text-sm flex items-center gap-2 hover:bg-docwise-700 aria-selected:!bg-docwise-600"
+            >
+              <File className="w-4 h-4" />
+              {document.title}
+            </Command.Item>
+          )
+        })}
       </Command.List>
     </Command.Dialog>
   )
